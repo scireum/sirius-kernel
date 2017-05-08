8c4d14a76ec7f93dec581688ab20b21a724df5ae
Dennis Heller <dhe@scireum.de>
2017-05-08 10:32:31 +0200

    Improves the Trie class

--- src/test/java/sirius/kernel/commons/TrieTest.java
+++ src/test/java/sirius/kernel/commons/TrieTest.java
@@ -1,14 +1,18 @@
 package sirius.kernel.commons;
 
+import com.google.common.collect.Sets;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class TrieTest {
-    @Test
-    public void isFilled() {
-        Trie<Integer> trie = Trie.create();
 
+    private Trie<Integer> trie;
+
+    @Before
+    public void createTrie() {
+        trie = Trie.create();
         trie.put("one", 1);
         trie.put("on", 2);
         trie.put("one1", 3);
@@ -16,7 +20,10 @@ public class TrieTest {
         trie.put("three", 5);
         trie.put("thrae", 6);
         trie.put("th", 7);
+    }
 
+    @Test
+    public void isFilled() {
         String check = "I'd like to have three beer please";
         Trie.ContainmentIterator<Integer> iter = trie.iterator();
 
@@ -37,5 +44,16 @@ public class TrieTest {
         assertEquals(null, trie.get("onx"));
         assertTrue(trie.containsKey("thrae"));
         assertFalse(trie.containsKey("thre"));
+    }
+
+    @Test
+    public void keySet() {
+        assertEquals(7, trie.size());
+        assertEquals(Sets.newHashSet("one", "on", "one1", "two", "three", "thrae", "th"), trie.keySet());
+        assertEquals(Sets.newHashSet("one", "on", "one1", "two", "three", "thrae", "th"),
+                     trie.getAllKeysBeginningWith(""));
+        assertEquals(Sets.newHashSet("one", "on", "one1"), trie.getAllKeysBeginningWith("on"));
+        assertEquals(Sets.newHashSet("three"), trie.getAllKeysBeginningWith("three"));
+        assertEquals(0, trie.getAllKeysBeginningWith("threee").size());
     }
 }
