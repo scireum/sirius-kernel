1424e8a189208bd07bc6aff99e2c2f16419dcd45
Fabian Welz <fwe@scireum.de>
2018-03-23 10:04:37 +0100

    Fixes bug in eviction of caches
    It actually removed every CacheEntry which did NOT reach its ttl, not the other way around

---
+++ src/test/java/sirius/kernel/cache/ManagedCacheSpec.groovy
@@ -0,0 +1,32 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.kernel.cache
+
+import sirius.kernel.BaseSpecification
+import sirius.kernel.commons.Wait
+
+class ManagedCacheSpec extends BaseSpecification {
+
+    def "test run eviction removes old entries"() {
+        given:
+        def cache = new ManagedCache("test-cache", null, null)
+        when:
+        cache.put("key1", "value1")
+        cache.put("key2", "value2")
+        Wait.seconds(11)
+        cache.put("key3", "value3")
+        cache.put("key4", "value4")
+        then:
+        cache.getSize() == 4
+        cache.runEviction()
+        cache.getSize() == 2
+        cache.get("key3") == "value3"
+        cache.get("key4") == "value4"
+    }
+}
