9ea90430b0c62996d1db59278060a9a08dd243c8
aha <aha@scireum.de>
2018-06-13 14:48:37 +0200

    Ensures that Settings.getConfigs returns a properly sorted list.

---
+++ src/test/java/sirius/kernel/settings/SettingsSpec.groovy
@@ -0,0 +1,34 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.kernel.settings
+
+import sirius.kernel.BaseSpecification
+import sirius.kernel.Sirius
+
+import java.util.stream.Collectors
+
+class SettingsSpec extends BaseSpecification {
+
+    def "inner configs are delivered in their given order"() {
+        when:
+        def keys = Sirius.getSettings().getConfigs("test-configs").stream().map({ c -> c.getString("value") }).collect(
+                Collectors.toList())
+        then:
+        keys == ["2", "1", "3"]
+    }
+
+    def "inner configs are delivered as sorted by their priority if given"() {
+        when:
+        def keys = Sirius.getSettings().getConfigs("test-configs-sorted").stream().map({ c -> c.getString("value") }).collect(
+                Collectors.toList())
+        then:
+        keys == ["1", "2", "3"]
+    }
+
+}
