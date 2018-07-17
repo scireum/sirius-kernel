da2e3484ef98c3ae7ec8971d0cb2d05e3fdf8b8e
aha <aha@scireum.de>
2018-07-17 12:11:52 +0200

    Removes the shutdown-hook as sirius-ipl already places one.
    If a shutdown-hook outside of sirius-ipl is needed, one has
    to provide it manually.

--- src/main/java/sirius/kernel/Sirius.java
+++ src/main/java/sirius/kernel/Sirius.java
@@ -399,8 +399,6 @@ public class Sirius {
         LOG.INFO(SEPARATOR_LINE);
         LOG.INFO("System is UP and RUNNING - %s", w.duration());
         LOG.INFO(SEPARATOR_LINE);
-
-        Runtime.getRuntime().addShutdownHook(new Thread(Sirius::stop));
     }
 
     /**
