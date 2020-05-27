9eb590b32f5939f7d3b889f04e9651c2998d5808
aha <aha@scireum.de>
2020-05-27 14:51:31 +0200

    Provides utilities for hashing and byte/char streams.
    We previously used guava for this, but the APIs are
    either deprecated or unstable.

---
+++ src/test/java/sirius/kernel/commons/StreamsTest.java
@@ -0,0 +1,48 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.kernel.commons;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.assertEquals;
+
+public class StreamsTest {
+
+    @Test
+    public void transferTest() throws IOException {
+        String testString = "Hello from the other side...";
+
+        ByteArrayInputStream in = new ByteArrayInputStream(testString.getBytes(StandardCharsets.UTF_8));
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Streams.transfer(in, out);
+
+        assertEquals(testString, new String(out.toByteArray(), StandardCharsets.UTF_8));
+    }
+
+    @Test
+    public void largeTransferTest() throws IOException {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < 10_000; i++) {
+            builder.append("Hello World");
+        }
+
+        String testString = builder.toString();
+
+        ByteArrayInputStream in = new ByteArrayInputStream(testString.getBytes(StandardCharsets.UTF_8));
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Streams.transfer(in, out);
+
+        assertEquals(testString, new String(out.toByteArray(), StandardCharsets.UTF_8));
+    }
+}
