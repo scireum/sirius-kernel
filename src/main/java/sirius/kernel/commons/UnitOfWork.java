f3189ce9bd26b446faa7aa680091ce4438413220
Sascha Bieberstein <sbi@scireum.de>
2020-06-10 14:55:32 +0200

    Adds runnable variant which is allowed to throw exceptions

---
+++ src/main/java/sirius/kernel/commons/UnitOfWork.java
@@ -0,0 +1,30 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.kernel.commons;
+
+/**
+ * Contains a unit of work which can be restarted.
+ */
+
+/**
+ * Implements the {@link Runnable} pattern but permits the function to throw an exception.
+ * <p>
+ * This sometimes might simplify exception handling. If this feature is not required use a plain
+ * {@link Runnable} instead.
+ */
+public interface UnitOfWork {
+
+    /**
+     * Starts or re-starts the block of code.
+     *
+     * @throws Exception the callee may throw any exception during the computation. Therefore the caller should
+     *                   implement proper error handling without relying on specific exception types.
+     */
+    void execute() throws Exception;
+}
