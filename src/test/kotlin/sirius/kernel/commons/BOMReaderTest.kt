88e27c4db9e43e5e846018ac0755e70811c11eb8
Christian Schierle <csc@scireum.de>
2022-10-07 08:54:55 +0200

    Ports changes in the Java BOMReaderTest to Kotlin
    Fixes: SIRI-632

--- src/test/kotlin/sirius/kernel/commons/BOMReaderTest.kt
+++ src/test/kotlin/sirius/kernel/commons/BOMReaderTest.kt
@@ -8,11 +8,12 @@
 
 package sirius.kernel.commons
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import java.io.ByteArrayInputStream
-import java.io.IOException
-import java.io.InputStreamReader
-import kotlin.test.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import java.io.*
+import java.nio.charset.StandardCharsets
 
 class BOMReaderTest {
 
@@ -27,51 +28,41 @@ class BOMReaderTest {
             'L'.code.toByte(),
             'O'.code.toByte()
         )
-        private val WITHOUT_BOM =
-            byteArrayOf('H'.code.toByte(), 'E'.code.toByte(), 'L'.code.toByte(), 'L'.code.toByte(), 'O'.code.toByte())
     }
 
     @Test
     @Throws(IOException::class)
     fun readBOM() {
-        val reader = BOMReader(InputStreamReader(ByteArrayInputStream(WITH_UTF8_BOM)))
-        assertEquals('H'.code.toLong(), reader.read().toLong())
-        assertEquals('E'.code.toLong(), reader.read().toLong())
+        val outputStream = ByteArrayOutputStream()
+        val writer = OutputStreamWriter(outputStream, StandardCharsets.UTF_8)
+        writer.write(Streams.UNICODE_BOM_CHARACTER)
+        writer.write("HELLO")
+        writer.flush()
+        val reader = BOMReader(InputStreamReader(ByteArrayInputStream(outputStream.toByteArray())))
+        Assertions.assertEquals('H'.code.toLong(), reader.read().toLong())
+        Assertions.assertEquals('E'.code.toLong(), reader.read().toLong())
     }
 
     @Test
     @Throws(IOException::class)
     fun readWithoutBOM() {
-        val reader = BOMReader(InputStreamReader(ByteArrayInputStream(WITHOUT_BOM)))
-        assertEquals('H'.code.toLong(), reader.read().toLong())
-        assertEquals('E'.code.toLong(), reader.read().toLong())
+        val outputStream = ByteArrayOutputStream()
+        val writer = OutputStreamWriter(outputStream, StandardCharsets.UTF_8)
+        writer.write("HELLO")
+        writer.flush()
+        val reader = BOMReader(InputStreamReader(ByteArrayInputStream(outputStream.toByteArray())))
+        Assertions.assertEquals('H'.code.toLong(), reader.read().toLong())
+        Assertions.assertEquals('E'.code.toLong(), reader.read().toLong())
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(ints = [1, 2, 5])
     @Throws(IOException::class)
-    fun readArray1BOM() {
+    fun readArrayBOM(length: Int) {
         val reader = BOMReader(InputStreamReader(ByteArrayInputStream(WITH_UTF8_BOM)))
-        val buffer = CharArray(1)
-        assertEquals(1, reader.read(buffer).toLong())
-        assertEquals('H'.code.toLong(), buffer[0].code.toLong())
-    }
-
-    @Test
-    @Throws(IOException::class)
-    fun readArray2BOM() {
-        val reader = BOMReader(InputStreamReader(ByteArrayInputStream(WITH_UTF8_BOM)))
-        val buffer = CharArray(2)
-        assertEquals(2, reader.read(buffer).toLong())
-        assertEquals('H'.code.toLong(), buffer[0].code.toLong())
-    }
-
-    @Test
-    @Throws(IOException::class)
-    fun readArray10BOM() {
-        val reader = BOMReader(InputStreamReader(ByteArrayInputStream(WITH_UTF8_BOM)))
-        val buffer = CharArray(10)
-        assertEquals(5, reader.read(buffer).toLong())
-        assertEquals('H'.code.toLong(), buffer[0].code.toLong())
+        val buffer = CharArray(length)
+        Assertions.assertEquals(length, reader.read(buffer))
+        Assertions.assertEquals('H'.code.toLong(), buffer[0].code.toLong())
     }
 
     @Test
@@ -79,7 +70,7 @@ class BOMReaderTest {
     fun readArrayWithoutBOM() {
         val reader = BOMReader(InputStreamReader(ByteArrayInputStream(WITH_UTF8_BOM)))
         val buffer = CharArray(2)
-        assertEquals(2, reader.read(buffer).toLong())
-        assertEquals('H'.code.toLong(), buffer[0].code.toLong())
+        Assertions.assertEquals(2, reader.read(buffer).toLong())
+        Assertions.assertEquals('H'.code.toLong(), buffer[0].code.toLong())
     }
 }
