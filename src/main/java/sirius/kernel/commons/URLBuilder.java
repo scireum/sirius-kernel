b9cf3cb298d7dc34c8da60ec63b152657aa6c151
Sascha Bieberstein <sbi@scireum.de>
2022-10-12 08:48:39 +0200

    Typos
    Fixes: SIRI-641

--- src/main/java/sirius/kernel/commons/URLBuilder.java
+++ src/main/java/sirius/kernel/commons/URLBuilder.java
@@ -44,7 +44,7 @@ public class URLBuilder {
     /**
      * Creates a new instance pre-filled with the given baseURL.
      *
-     * @param baseURL the base url to stat with in a form like <tt>http://somehost.com</tt> or
+     * @param baseURL the base URL to stat with in a form like <tt>http://somehost.com</tt> or
      *                <tt>http://somehost.com?some=parameter</tt>
      */
     public URLBuilder(@Nonnull String baseURL) {
@@ -75,7 +75,7 @@ public class URLBuilder {
     }
 
     /**
-     * Adds a path part to the url.
+     * Adds a path part to the URL.
      * <p>
      * Once the first parameter has been added, the path can no longer be modified. Also, the part itself can (but
      * shouldn't) contain parameters, usually led by an initial question mark.
@@ -90,7 +90,7 @@ public class URLBuilder {
         }
 
         if (questionMark.isToggled()) {
-            throw new IllegalStateException(Strings.apply("Cannot add '%s'! Parameters where already added to: '%s'.",
+            throw new IllegalStateException(Strings.apply("Cannot add '%s'! Parameters were already added to: '%s'.",
                                                           uriPartToAdd,
                                                           url));
         }
@@ -109,7 +109,7 @@ public class URLBuilder {
     }
 
     /**
-     * Adds multiple path parts to the url.
+     * Adds multiple path parts to the URL.
      * <p>
      * Once the first parameter has been added, the path can no longer be modified. Also, the part itself can (but
      * shouldn't) contain parameters, usually led by an initial question mark.
@@ -124,7 +124,7 @@ public class URLBuilder {
     }
 
     /**
-     * Adds a path part to the url.
+     * Adds a path part to the URL.
      * <p>
      * Once the first parameter has been added, the path can no longer be modified. Also, the part itself can (but
      * shouldn't) contain parameters, usually led by an initial question mark.
@@ -143,7 +143,7 @@ public class URLBuilder {
             if (Strings.isFilled(uriPart)) {
                 if (questionMark.isToggled()) {
                     throw new IllegalStateException(Strings.apply(
-                            "Cannot add '%s'! Parameters where already added to: '%s'.",
+                            "Cannot add '%s'! Parameters were already added to: '%s'.",
                             uriPart,
                             url));
                 }
@@ -158,10 +158,10 @@ public class URLBuilder {
     }
 
     /**
-     * Adds a parameter to the url.
+     * Adds a parameter to the URL.
      *
      * @param key   the name of the parameter
-     * @param value the value of the parameter which will be url encoded. If the given value is <tt>null</tt> an empty
+     * @param value the value of the parameter which will be URL-encoded. If the given value is <tt>null</tt> an empty
      *              parameter will be added.
      * @return the builder itself for fluent method calls
      */
@@ -170,11 +170,11 @@ public class URLBuilder {
     }
 
     /**
-     * Adds a parameter to the url.
+     * Adds a parameter to the URL.
      *
      * @param key       the name of the parameter
      * @param value     the value of the parameter . If the given value is <tt>null</tt> an empty parameter will be added.
-     * @param urlEncode <tt>true</tt> if the given value should be url encoded before adding
+     * @param urlEncode <tt>true</tt> if the given value should be URL-encoded before adding
      * @return the builder itself for fluent method calls
      */
     public URLBuilder addParameter(@Nonnull String key, @Nullable Object value, boolean urlEncode) {
@@ -200,9 +200,9 @@ public class URLBuilder {
     }
 
     /**
-     * Builds the url and returns it as a string.
+     * Builds the URL and returns it as a string.
      *
-     * @return the url that was built as string
+     * @return the URL that was built as string
      */
     public String build() {
         return url.toString();
@@ -211,9 +211,9 @@ public class URLBuilder {
     /**
      * Creates a {@link URL URL object} from the resulting string of this builder.
      * <p>
-     * Only works if the url contains a valid protocol.
+     * Only works if the URL contains a valid protocol.
      *
-     * @return the url that was built as a {@link URL URL object}
+     * @return the URL that was built as a {@link URL URL object}
      * @throws IllegalStateException should only happen if no protocol or an invalid protocol has been given
      */
     public URL asURL() {
@@ -227,10 +227,10 @@ public class URLBuilder {
     /**
      * Creates a {@link URI URI object} from the resulting string of this builder.
      * <p>
-     * Only works if the url contains a valid protocol.
+     * Only works if the URL contains a valid protocol.
      * This internally uses {@link #asURL()} as we only want valid URLs to be build as URI.
      *
-     * @return the url that was built as a {@link URI URI object}
+     * @return the URL that was built as a {@link URI URI object}
      * @throws IllegalStateException should only happen if no protocol or an invalid protocol has been given
      */
     public URI asURI() {
