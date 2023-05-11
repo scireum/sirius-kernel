86f612b0a4a07dc5deb06a0501f1eb5f48d337ff
Christian Schierle <csc@scireum.de>
2023-05-11 12:10:07 +0200

    Stops the sirius environment after test class was executed
    Fixes: SIRI-723

--- src/test/java/sirius/kernel/SiriusExtension.java
+++ src/test/java/sirius/kernel/SiriusExtension.java
@@ -8,6 +8,7 @@
 
 package sirius.kernel;
 
+import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
@@ -15,11 +16,12 @@ import sirius.kernel.async.CallContext;
 
 /**
  * JUnit 5 extension to support Sirius framework lifecycle. Annotated test classes will be provisioned with
- * a running framework and a cleared {@link CallContext} before each test.
+ * a running framework and a cleared {@link CallContext} before each test. The framework is stopped after execution of
+ * the test class has completed.
  * <p>
  * Note: This currently does not support {@link sirius.kernel.Scenario scenarios}.
  */
-public class SiriusExtension implements BeforeAllCallback, BeforeEachCallback {
+public class SiriusExtension implements BeforeAllCallback, BeforeEachCallback, AfterAllCallback {
 
     @Override
     public void beforeAll(ExtensionContext context) {
@@ -29,5 +31,10 @@ public class SiriusExtension implements BeforeAllCallback, BeforeEachCallback {
     @Override
     public void beforeEach(ExtensionContext context) throws Exception {
         CallContext.initialize();
+    }
+
+    @Override
+    public void afterAll(ExtensionContext context) throws Exception {
+        TestHelper.tearDown(SiriusExtension.class);
     }
 }
