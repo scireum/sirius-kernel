4b1aef1e58a1ac094c3e933785d3b1c6b23aedcd
Christian Schierle <csc@scireum.de>
2023-05-11 12:15:25 +0200

    Revert "Upgrades sonarqube plugin for Gradle 8 support"
    This reverts commit aa186baa

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@
 plugins {
     id("java-library")
     id("com.scireum.sirius-parent") version "11.0.3"
-    id("org.sonarqube") version "4.0.0.2929"
+    id("org.sonarqube") version "3.4.0.2513"
 }
 
 dependencies {
