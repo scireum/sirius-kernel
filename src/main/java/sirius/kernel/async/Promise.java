f5ddea0d69365bf850150f5b0335532794d95c95
Matthias Keck <mke@scireum.de>
2023-06-29 08:35:21 +0200

    Deprecates method as it actually is not required
    - to be removed later

--- src/main/java/sirius/kernel/async/Promise.java
+++ src/main/java/sirius/kernel/async/Promise.java
@@ -323,7 +323,9 @@ public class Promise<V> {
      * Returns this promise as a future.
      *
      * @return this promise as future
+     * @deprecated Usage of this method is discouraged. Wrap and use {@link CombinedFuture#asFuture()}} instead.
      */
+    @Deprecated(forRemoval = true)
     public Future asFuture() {
         CombinedFuture result = new CombinedFuture();
         result.add(this);
