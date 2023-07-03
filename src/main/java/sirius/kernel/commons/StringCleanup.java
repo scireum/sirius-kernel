77928dbe1e62c02913d8d1d181e5ac4e75a0001d
Andreas Haufler <aha@scireum.de>
2023-07-03 14:00:39 +0200

    Improves some JavaDocs.

--- src/main/java/sirius/kernel/commons/StringCleanup.java
+++ src/main/java/sirius/kernel/commons/StringCleanup.java
@@ -11,17 +11,23 @@ package sirius.kernel.commons;
 import javax.annotation.Nonnull;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.function.UnaryOperator;
 import java.util.regex.Pattern;
 
+/**
+ * Provides various methods to clean-up or reduce strings.
+ *
+ * @see Strings#cleanup(String, Iterable)
+ */
 public class StringCleanup {
 
-    public static final Pattern PATTERN_CONTROL_CHARACTERS = Pattern.compile("\\p{Cntrl}");
-    public static final Pattern PATTERN_WHITESPACE = Pattern.compile("\\s");
-    public static final Pattern PATTERN_WHITESPACES = Pattern.compile("\\s+");
-    public static final Pattern PATTERN_PUNCTUATION = Pattern.compile("\\p{Punct}");
-    public static final Pattern PATTERN_NON_ALPHA_NUMERIC = Pattern.compile("([^\\p{L}\\d])");
-    public static final Pattern PATTERN_NON_LETTER = Pattern.compile("\\P{L}");
-    public static final Pattern PATTERN_NON_DIGIT = Pattern.compile("\\D");
+    private static final Pattern PATTERN_CONTROL_CHARACTERS = Pattern.compile("\\p{Cntrl}");
+    private static final Pattern PATTERN_WHITESPACE = Pattern.compile("\\s");
+    private static final Pattern PATTERN_WHITESPACES = Pattern.compile("\\s+");
+    private static final Pattern PATTERN_PUNCTUATION = Pattern.compile("\\p{Punct}");
+    private static final Pattern PATTERN_NON_ALPHA_NUMERIC = Pattern.compile("([^\\p{L}\\d])");
+    private static final Pattern PATTERN_NON_LETTER = Pattern.compile("\\P{L}");
+    private static final Pattern PATTERN_NON_DIGIT = Pattern.compile("\\D");
 
     private static final Map<Integer, String> unicodeMapping = new TreeMap<>();
 
@@ -65,76 +71,182 @@ public class StringCleanup {
         }
     }
 
+    /**
+     * Removes all control characters from the given string.
+     *
+     * @param input the input to process
+     * @return the resulting string
+     */
     @Nonnull
     public static String removeControlCharacters(@Nonnull String input) {
         return PATTERN_CONTROL_CHARACTERS.matcher(input).replaceAll("");
     }
 
+    /**
+     * Replaces all control characters with a whitespace character.
+     * <p>
+     * Most probably this should be followed by {@link #reduceWhitespace(String)}.
+     *
+     * @param input the input to process
+     * @return the resulting string
+     */
     @Nonnull
     public static String replaceControlCharacters(@Nonnull String input) {
         return PATTERN_CONTROL_CHARACTERS.matcher(input).replaceAll(" ");
     }
 
+    /**
+     * Removes all punctuation characters from the given string.
+     *
+     * @param input the input to process
+     * @return the resulting string
+     */
     @Nonnull
     public static String removePunctuation(@Nonnull String input) {
         return PATTERN_PUNCTUATION.matcher(input).replaceAll("");
     }
 
+    /**
+     * Replaces all punctuation characters with a whitespace character.
+     * <p>
+     * Most probably this should be followed by {@link #reduceWhitespace(String)}.
+     *
+     * @param input the input to process
+     * @return the resulting string
+     */
     @Nonnull
     public static String replacePunctuation(@Nonnull String input) {
         return PATTERN_PUNCTUATION.matcher(input).replaceAll(" ");
     }
 
+    /**
+     * Removes all non-alphanumeric (0-9 a-z A-Z) characters from the given string.
+     *
+     * @param input the input to process
+     * @return the resulting string
+     */
     @Nonnull
     public static String removeNonAlphaNumeric(@Nonnull String input) {
         return PATTERN_NON_ALPHA_NUMERIC.matcher(input).replaceAll("");
     }
 
+    /**
+     * Replaces all non-alphanumeric characters with a whitespace character.
+     * <p>
+     * Most probably this should be followed by {@link #reduceWhitespace(String)}.
+     *
+     * @param input the input to process
+     * @return the resulting string
+     */
     @Nonnull
     public static String replaceNonAlphaNumeric(@Nonnull String input) {
         return PATTERN_NON_ALPHA_NUMERIC.matcher(input).replaceAll(" ");
     }
 
+    /**
+     * Removes all non-letter (<tt>\P{L}</tt>) characters from the given string.
+     *
+     * @param input the input to process
+     * @return the resulting string
+     */
     @Nonnull
     public static String removeNonLetter(@Nonnull String input) {
         return PATTERN_NON_LETTER.matcher(input).replaceAll("");
     }
 
+    /**
+     * Replaces all non-letter (<tt>\P{L}</tt>) characters with a whitespace character.
+     * <p>
+     * Most probably this should be followed by {@link #reduceWhitespace(String)}.
+     *
+     * @param input the input to process
+     * @return the resulting string
+     */
     @Nonnull
     public static String replaceNonLetter(@Nonnull String input) {
         return PATTERN_NON_LETTER.matcher(input).replaceAll(" ");
     }
 
+    /**
+     * Removes all non-digit (<tt>\D</tt>) characters from the given string.
+     *
+     * @param input the input to process
+     * @return the resulting string
+     */
     @Nonnull
     public static String removeNonDigit(@Nonnull String input) {
         return PATTERN_NON_DIGIT.matcher(input).replaceAll("");
     }
 
+    /**
+     * Replaces all non-digit (<tt>\D</tt>) characters with a whitespace character.
+     * <p>
+     * Most probably this should be followed by {@link #reduceWhitespace(String)}.
+     *
+     * @param input the input to process
+     * @return the resulting string
+     */
     @Nonnull
     public static String replaceNonDigit(@Nonnull String input) {
         return PATTERN_NON_DIGIT.matcher(input).replaceAll(" ");
     }
 
+    /**
+     * Removes all whitespace characters from the given string.
+     *
+     * @param input the input to process
+     * @return the resulting string
+     */
     @Nonnull
     public static String removeWhitespace(@Nonnull String input) {
         return PATTERN_WHITESPACE.matcher(input).replaceAll("");
     }
 
+    /**
+     * Replaces multiple whitespace characters with a single whitespace character (" ").
+     *
+     * @param input the input to process
+     * @return the resulting string
+     */
     @Nonnull
     public static String reduceWhitespace(@Nonnull String input) {
         return PATTERN_WHITESPACES.matcher(input).replaceAll(" ");
     }
 
+    /**
+     * Trims the given string.
+     * <p>
+     * This is essentially an alias for {@code String::trim}.
+     *
+     * @param input the input to process
+     * @return the resulting string
+     */
     @Nonnull
     public static String trim(@Nonnull String input) {
         return input.trim();
     }
 
+    /**
+     * Converts the string to lowercase.
+     * <p>
+     * This is essentially an alias for {@code String::lowercase}.
+     *
+     * @param input the input to process
+     * @return the resulting string
+     */
     @Nonnull
     public static String lowercase(@Nonnull String input) {
         return input.toLowerCase();
     }
 
+    /**
+     * Converts the string to uppercase.
+     * <p>
+     * This is essentially an alias for {@code String::uppercase}.
+     *
+     * @param input the input to process
+     * @return the resulting string
+     */
     @Nonnull
     public static String uppercase(@Nonnull String input) {
         return input.toUpperCase();
