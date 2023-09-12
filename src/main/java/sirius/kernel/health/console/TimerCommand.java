045ab982075444951e017a3a90d411e47231b7de
Jakob Vogel <jvo@scireum.de>
2023-09-12 16:41:49 +0200

    Improves variable names ✨
    `ts` → `timers`
    `params` → `parameters`

--- src/main/java/sirius/kernel/health/console/TimerCommand.java
+++ src/main/java/sirius/kernel/health/console/TimerCommand.java
@@ -34,52 +34,53 @@ public class TimerCommand implements Command {
     private static final String USAGE = "Usage: timer all|oneMinute|tenMinutes|oneHour|everyDay <hour>";
 
     @Part
-    private Timers ts;
+    private Timers timers;
 
     @Override
-    public void execute(Output output, String... params) throws Exception {
-        if (params.length == 0) {
+    public void execute(Output output, String... parameters) throws Exception {
+        if (parameters.length == 0) {
             output.line(USAGE);
-        } else if (!ACCEPTED_PARAMS.contains(params[0])) {
-            output.apply("'%s' is not an accepted parameter!", params[0]);
+        } else if (!ACCEPTED_PARAMS.contains(parameters[0])) {
+            output.apply("'%s' is not an accepted parameter!", parameters[0]);
             output.line(USAGE);
         } else {
-            if ("all".equalsIgnoreCase(params[0]) || "oneMinute".equalsIgnoreCase(params[0])) {
+            if ("all".equalsIgnoreCase(parameters[0]) || "oneMinute".equalsIgnoreCase(parameters[0])) {
                 output.line("Executing one minute timers...");
-                ts.runOneMinuteTimers();
+                timers.runOneMinuteTimers();
             }
-            if ("all".equalsIgnoreCase(params[0]) || "tenMinutes".equalsIgnoreCase(params[0])) {
+            if ("all".equalsIgnoreCase(parameters[0]) || "tenMinutes".equalsIgnoreCase(parameters[0])) {
                 output.line("Executing ten minute timers...");
-                ts.runTenMinuteTimers();
+                timers.runTenMinuteTimers();
             }
-            if ("all".equalsIgnoreCase(params[0]) || "oneHour".equalsIgnoreCase(params[0])) {
+            if ("all".equalsIgnoreCase(parameters[0]) || "oneHour".equalsIgnoreCase(parameters[0])) {
                 output.line("Executing one hour timers...");
-                ts.runOneHourTimers();
+                timers.runOneHourTimers();
             }
-            if ("everyDay".equalsIgnoreCase(params[0])) {
-                int currentHour = Values.of(params).at(1).asInt(25);
+            if ("everyDay".equalsIgnoreCase(parameters[0])) {
+                int currentHour = Values.of(parameters).at(1).asInt(25);
                 output.line("Executing daily timers for hour: " + currentHour);
-                ts.runEveryDayTimers(currentHour);
+                timers.runEveryDayTimers(currentHour);
             }
         }
 
         output.blankLine();
         output.line("System Timers - Last Execution");
         output.separator();
-        output.apply(LINE_FORMAT, "One-Minute", ts.getLastOneMinuteExecution());
-        output.apply(LINE_FORMAT, "Ten-Minutes", ts.getLastTenMinutesExecution());
-        output.apply(LINE_FORMAT, "One-Hour", ts.getLastHourExecution());
+        output.apply(LINE_FORMAT, "One-Minute", timers.getLastOneMinuteExecution());
+        output.apply(LINE_FORMAT, "Ten-Minutes", timers.getLastTenMinutesExecution());
+        output.apply(LINE_FORMAT, "One-Hour", timers.getLastHourExecution());
         output.separator();
         output.blankLine();
         output.line("Daily Tasks");
         output.separator();
 
-        ts.getDailyTasks()
-          .stream()
-          .map(task -> Tuple.create(Sirius.getSettings().getInt(Timers.TIMER_DAILY_PREFIX + task.getConfigKeyName()),
-                                    task.getConfigKeyName()))
-          .sorted(Comparator.comparingInt(Tuple::getFirst))
-          .forEach(hourAndTask -> output.apply("%2sh: %s", hourAndTask.getFirst(), hourAndTask.getSecond()));
+        timers.getDailyTasks()
+              .stream()
+              .map(task -> Tuple.create(Sirius.getSettings()
+                                              .getInt(Timers.TIMER_DAILY_PREFIX + task.getConfigKeyName()),
+                                        task.getConfigKeyName()))
+              .sorted(Comparator.comparingInt(Tuple::getFirst))
+              .forEach(hourAndTask -> output.apply("%2sh: %s", hourAndTask.getFirst(), hourAndTask.getSecond()));
 
         output.separator();
     }
