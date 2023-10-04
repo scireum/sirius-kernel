c3bf24b8bebdd249fcc23efcbc70c933526e5992
mko <mko@scireum.de>
2023-10-04 13:20:10 +0200

    Adds a new helper class for dates
    Fixes: OX-10394

---
+++ src/main/java/sirius/kernel/commons/Dates.java
@@ -0,0 +1,16 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.kernel.commons;
+
+/**
+ * Provides various helper methods for dealing with dates.
+ */
+public class Dates {
+
+}
