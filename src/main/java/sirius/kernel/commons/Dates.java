f9286a0c62e4057bbe2441f67e4a81c5ea7aa86a
mko <mko@scireum.de>
2023-10-04 13:45:35 +0200

    Adds methods (incl. Tests) which compute the latest date of the given dates
    Fixes: OX-10394

--- src/main/java/sirius/kernel/commons/Dates.java
+++ src/main/java/sirius/kernel/commons/Dates.java
@@ -8,9 +8,45 @@
 
 package sirius.kernel.commons;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * Provides various helper methods for dealing with dates.
  */
 public class Dates {
 
+    /**
+     * All methods are static, therefore no instances are required.
+     */
+    private Dates() {
+    }
+
+    /**
+     * Computes the latest date-time of the given date-time objects.
+     *
+     * @param dateTimes the date-time objects to compare
+     * @return the latest date-time object or <tt>null</tt> if no date is given
+     */
+    public static LocalDateTime computeLatestDate(LocalDateTime... dateTimes) {
+        return Arrays.stream(dateTimes)
+                     .filter(Objects::nonNull)
+                     .max(LocalDateTime::compareTo)
+                     .orElse(null);
+    }
+
+    /**
+     * Computes the latest date of the given date objects.
+     *
+     * @param dates the dates to compare
+     * @return the latest date object or <tt>null</tt> if no date is given
+     */
+    public static LocalDate computeLatestDate(LocalDate... dates) {
+        return Arrays.stream(dates)
+                     .filter(Objects::nonNull)
+                     .max(LocalDate::compareTo)
+                     .orElse(null);
+    }
 }
---
+++ src/test/kotlin/sirius/kernel/commons/DatesTest.kt
@@ -0,0 +1,36 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.kernel.commons
+
+import java.time.LocalDate
+import java.time.LocalDateTime
+import kotlin.test.*
+
+/**
+ * Tests the [Dates] class.
+ */
+class DatesTest {
+    @Test
+    fun computeLatestDateTime() {
+        val theDayBeforeYesterday = LocalDateTime.now().minusDays(2)
+        val yesterday = LocalDateTime.now().minusDays(1)
+        val now = LocalDateTime.now()
+        assertEquals(now, Dates.computeLatestDate(null, theDayBeforeYesterday, now, yesterday))
+        assertEquals(null, null)
+    }
+
+    @Test
+    fun computeLatestDate() {
+        val theDayBeforeYesterday = LocalDate.now().minusDays(2)
+        val yesterday = LocalDate.now().minusDays(1)
+        val now = LocalDate.now()
+        assertEquals(now, Dates.computeLatestDate(null, theDayBeforeYesterday, now, yesterday))
+        assertEquals(null, null)
+    }
+}
