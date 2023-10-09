cee9cfb1ab62a3a6c95e833a0ea62705ec4ea38e
Sascha Bieberstein <sbi@scireum.de>
2023-10-09 15:23:13 +0200

    Converts syntax of test to Kotlin
    Fixes: OX-10357

--- src/test/kotlin/sirius/kernel/async/TasksTest.kt
+++ src/test/kotlin/sirius/kernel/async/TasksTest.kt
@@ -8,108 +8,130 @@
 
 package sirius.kernel.async
 
-class TasksTest extends BaseSpecification {
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import sirius.kernel.SiriusExtension
+import sirius.kernel.commons.ValueHolder
+import sirius.kernel.di.std.Part
+import java.time.Duration
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+import kotlin.test.assertTrue
 
-    @Part
-    public static Tasks tasks
+/**
+ * Tests the [Tasks] class.
+ */
+@ExtendWith(SiriusExtension::class)
+class TasksTest {
 
-            static final Duration DEFAULT_TIMEOUT = Duration.ofSeconds(10)
-
-    def "An executor executes work in the calling thread when full"() {
-        given: "a future the synchronize the threads"
-        Future thread2Finished = new Future()
-        and: "a place to store the thread id of the thread which executes the 2nd task"
-        ValueHolder<Long> executorThread = ValueHolder.of(null)
-        when: "we start one tasks which blocks the executor"
-        Future task1Future = tasks.executor("test-limited").start({ thread2Finished.await(DEFAULT_TIMEOUT) })
-        and: "we start another task for the blocked executor"
-        Future task2Future = tasks.executor("test-limited").start({
-            executorThread.set(Thread.currentThread().getId())
+    @Test
+    fun `An executor executes work in the calling thread when full`() {
+        // a future to synchronize the threads
+        val thread2Finished: Future = Future()
+        // a place to store the thread id of the thread which executes the 2nd task
+        val executorThread: ValueHolder<Long> = ValueHolder.of(null)
+        // we start one tasks which blocks the executor
+        val task1Future: Future = tasks.executor("test-limited").start {
+            thread2Finished.await(DEFAULT_TIMEOUT)
+        }
+        // we start another task for the blocked executor
+        val task2Future: Future = tasks.executor("test-limited").start {
+            executorThread.set(Thread.currentThread().id)
             thread2Finished.success()
-        })
-        and: "we wait until all background tasks are done"
+        }
+        // we wait until all background tasks are done
         task1Future.await(DEFAULT_TIMEOUT)
         task2Future.await(DEFAULT_TIMEOUT)
-        then: "we expect the second task to be executed in the calling thread"
-        Thread.currentThread().getId() == executorThread.get()
+        // we expect the second task to be executed in the calling thread
+        assertEquals(Thread.currentThread().id, executorThread.get())
     }
 
-    def "An executor drops tasks (if possible) when full"() {
-        given: "a future the synchronize the threads"
-        Future thread2Finished = new Future()
-        and: "a place to store the fact that the task was dropped"
-        ValueHolder<Boolean> dropped = ValueHolder.of(null)
-        when: "we start one tasks which blocks the executor"
-        Future task1Future = tasks.executor("test-limited").start({
+    @Test
+    fun `An executor drops tasks (if possible) when full`() {
+        // a future to synchronize the threads
+        val thread2Finished: Future = Future()
+        // a place to store the fact that the task was dropped
+        val dropped: ValueHolder<Boolean> = ValueHolder.of(null)
+        // we start one tasks which blocks the executor
+        val task1Future: Future = tasks.executor("test-limited").start {
             thread2Finished.await(DEFAULT_TIMEOUT)
-        })
-        and: "we start another task for the blocked executor"
-        Future task2Future = tasks.executor("test-limited").dropOnOverload({
+        }
+        // we start another task for the blocked executor
+        val task2Future: Future = tasks.executor("test-limited").dropOnOverload {
             dropped.set(true)
             thread2Finished.success()
-        }).start({
+        }.start {
             dropped.set(false)
             thread2Finished.success()
-        })
-        and: "we wait until all background tasks are done"
+        }
+        // we wait until all background tasks are done
         task1Future.await(DEFAULT_TIMEOUT)
         task2Future.await(DEFAULT_TIMEOUT)
-        then: "we expect the task the be dropped"
-        dropped.get() == true
-        and: "we expect the task1 to have successfully completed"
-        task1Future.isSuccessful()
-        and: "we expect the task1 to have failed"
-        task2Future.isFailed()
+        // we expect the task to be dropped
+        assertTrue { dropped.get() }
+        // we expect the task1 to have successfully completed
+        assertTrue { task1Future.isSuccessful }
+        // we expect the task1 to have failed
+        assertTrue { task2Future.isFailed }
     }
 
-    def "An executor uses its work queue when full"() {
-        given: "futures the synchronize the threads"
-        Future thread2Started = new Future()
-        Future thread2Finished = new Future()
-        and: "a place to store the thread ids which executed the task"
-        ValueHolder<Long> task1Thread = ValueHolder.of(null)
-        ValueHolder<Long> task2Thread = ValueHolder.of(null)
-        when: "we start one tasks which blocks the executor"
-        tasks.executor("test-unlimited").start({
-            task1Thread.set(Thread.currentThread().getId())
+    @Test
+    fun `An executor uses its work queue when full`() {
+        // futures to synchronize the threads
+        val thread2Started: Future = Future()
+        val thread2Finished: Future = Future()
+        // a place to store the thread ids which executed the task
+        val task1Thread: ValueHolder<Long> = ValueHolder.of(null)
+        val task2Thread: ValueHolder<Long> = ValueHolder.of(null)
+        // we start one tasks which blocks the executor
+        tasks.executor("test-unlimited").start {
+            task1Thread.set(Thread.currentThread().id)
             thread2Started.await(DEFAULT_TIMEOUT)
-        })
-        and: "we start another task for the blocked executor"
-        tasks.executor("test-unlimited").start({
-            task2Thread.set(Thread.currentThread().getId())
+        }
+        // we start another task for the blocked executor
+        tasks.executor("test-unlimited").start {
+            task2Thread.set(Thread.currentThread().id)
             thread2Finished.success()
-        })
+        }
         thread2Started.success()
-        and: "we wait until all background tasks are done"
+        // we wait until all background tasks are done
         thread2Finished.await(DEFAULT_TIMEOUT)
-        then: "we expect both tasks to be executed in the same thread"
-        task1Thread.get() == task2Thread.get()
-        then: "we expect both tasks not to be executed in the main thread"
-        Thread.currentThread().getId() != task1Thread.get()
+        // we expect both tasks to be executed in the same thread
+        assertEquals(task1Thread.get(), task2Thread.get())
+        // we expect both tasks not to be executed in the main thread
+        assertNotEquals(Thread.currentThread().id, task1Thread.get())
     }
 
-    def "An executor uses parallel threads if possible and required"() {
-        given: "a future the synchronize the threads"
-        Future thread2Finished = new Future()
-        and: "a place to store the thread ids which executed the task"
-        ValueHolder<Long> task1Thread = ValueHolder.of(null)
-        ValueHolder<Long> task2Thread = ValueHolder.of(null)
-        when: "we start one tasks which blocks the executor"
-        tasks.executor("test-parallel").start({
-            task1Thread.set(Thread.currentThread().getId())
+    @Test
+    fun `An executor uses parallel threads if possible and required`() {
+        // a future to synchronize the threads
+        val thread2Finished: Future = Future()
+        // a place to store the thread ids which executed the task
+        val task1Thread: ValueHolder<Long> = ValueHolder.of(null)
+        val task2Thread: ValueHolder<Long> = ValueHolder.of(null)
+        // we start one tasks which blocks the executor
+        tasks.executor("test-parallel").start {
+            task1Thread.set(Thread.currentThread().id)
             thread2Finished.await(DEFAULT_TIMEOUT)
-        })
-        and: "we start another task for the blocked executor"
-        tasks.executor("test-parallel").start({
-            task2Thread.set(Thread.currentThread().getId())
+        }
+        // we start another task for the blocked executor
+        tasks.executor("test-parallel").start {
+            task2Thread.set(Thread.currentThread().id)
             thread2Finished.success()
-        })
-        and: "we wait until all background tasks are done"
+        }
+        // we wait until all background tasks are done
         thread2Finished.await(DEFAULT_TIMEOUT)
-        then: "we expect both tasks to be executed in different threads"
-        task1Thread.get() != task2Thread.get()
-        then: "we expect both tasks not to be executed in the main thread"
-        Thread.currentThread().getId() != task1Thread.get()
+        // we expect both tasks to be executed in different threads
+        assertNotEquals(task1Thread.get(), task2Thread.get())
+        // we expect both tasks not to be executed in the main thread
+        assertNotEquals(Thread.currentThread().id, task1Thread.get())
     }
 
+    companion object {
+        val DEFAULT_TIMEOUT: Duration = Duration.ofSeconds(10)
+
+        @Part
+        @JvmStatic
+        private lateinit var tasks: Tasks
+    }
 }
