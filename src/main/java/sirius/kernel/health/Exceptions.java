2dced02e5c80b56dbca198c0b4760eeb83666d77
Marcus <106659278+mko-sci@users.noreply.github.com>
2024-05-16 14:49:41 +0200

    Removes double white sprace from stack trace
    Co-authored-by: Sascha Bieberstein <sabieber@users.noreply.github.com>

--- src/main/java/sirius/kernel/health/Exceptions.java
+++ src/main/java/sirius/kernel/health/Exceptions.java
@@ -529,6 +529,6 @@ public class Exceptions {
      */
     private static void appendStackTrace(StringBuilder stringBuilder, Throwable throwable) {
         Arrays.stream(throwable.getStackTrace())
-              .forEach(stackTraceElement -> stringBuilder.append("\t").append("at  ").append(stackTraceElement).append("\n"));
+              .forEach(stackTraceElement -> stringBuilder.append("\t").append("at ").append(stackTraceElement).append("\n"));
     }
 }
